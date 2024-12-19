cc54e8b17f8c730a943136ef4902a6b401db70e8
Dan Newman <dnewms@umich.edu>
2024-12-19 15:30:55 -0500

    lots o changes

---
+++ src/lib/nav.ts
@@ -0,0 +1,15 @@
+// src/config/navigation.ts
+export const subNavConfig = {
+    // Match WordPress slugs exactly to edit their subnav menus
+    'about': [
+      { title: "Team", href: "/about-us/team" },
+      { title: "Mission", href: "/about-us/mission" }
+    ],
+    'research': [
+
+    ],
+    'academics/undergraduate': [
+
+    ]
+  };
+  
