f80c92f669f19fb186518f6e892217debce50ff0
Dan Newman <dan@dannewman.org>
2024-12-23 10:20:48 -0500

    moving mainnav config local

--- src/lib/nav.ts
+++ src/lib/nav.ts
@@ -1,10 +1,6 @@
 // src/config/navigation.ts
 export const subNavConfig = {
     // Match WordPress slugs exactly to edit their subnav menus
-    'about$': [
-      { title: "Team", href: "/about-us/team" },
-      { title: "Mission", href: "/about-us/mission" }
-    ],
     // Only match exact 'research' path, not children
     'research$': [
       { title: "Focus Areas", href: "/research/focus-areas" },
