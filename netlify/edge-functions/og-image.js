c179468893a001ccc3d4cdcb7c9fcf1d471c6f7a
Dan Newman <dan@dannewman.org>
2025-01-13 07:48:31 -0500

    edge function test

--- netlify/edge-functions/og-image.js
+++ netlify/edge-functions/og-image.js
@@ -12,93 +12,105 @@ export default async (request, context) => {
     const backgroundData = await fetch(new URL(bgImage, request.url)).then(res => res.arrayBuffer());
 
     return new ImageResponse(
-      (
-        <div
-          style={{
+      {
+        type: 'div',
+        props: {
+          style: {
             height: '100%',
             width: '100%',
             display: 'flex',
             flexDirection: 'column',
             position: 'relative',
-          }}
-        >
-          {/* Background Image */}
-          <img
-            src={backgroundData}
-            alt=""
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-            }}
-          />
-          
-          {/* Dark Overlay */}
-          <div
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              background: 'rgba(0, 39, 76, 0.7)', // Michigan Blue with opacity
-            }}
-          />
-
-          {/* Logo */}
-          <img
-            src={logoData}
-            alt="Michigan Robotics"
-            style={{
-              position: 'absolute',
-              top: '40px',
-              left: '40px',
-              width: '200px', // Adjust size as needed
-              height: 'auto',
-            }}
-          />
-
-          {/* Content */}
-          <div
-            style={{
-              position: 'relative',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'flex-start',
-              justifyContent: 'flex-end',
-              padding: '60px',
-              height: '100%',
-            }}
-          >
-            <h1
-              style={{
-                fontSize: '60px',
-                fontWeight: 'bold',
-                color: '#FFCB05', // Michigan Yellow
-                margin: '0 0 20px',
-                lineHeight: 1.2,
-                textShadow: '2px 2px 4px rgba(0,0,0,0.5)',
-              }}
-            >
-              {title}
-            </h1>
-            <p
-              style={{
-                fontSize: '32px',
-                color: 'white',
-                margin: 0,
-                lineHeight: 1.4,
-                textShadow: '1px 1px 2px rgba(0,0,0,0.5)',
-              }}
-            >
-              {description}
-            </p>
-          </div>
-        </div>
-      ),
+          },
+          children: [
+            {
+              type: 'img',
+              props: {
+                src: backgroundData,
+                alt: '',
+                style: {
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                }
+              }
+            },
+            {
+              type: 'div',
+              props: {
+                style: {
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  width: '100%',
+                  height: '100%',
+                  background: 'rgba(0, 39, 76, 0.7)', // Michigan Blue with opacity
+                }
+              }
+            },
+            {
+              type: 'img',
+              props: {
+                src: logoData,
+                alt: 'Michigan Robotics',
+                style: {
+                  position: 'absolute',
+                  top: '40px',
+                  left: '40px',
+                  width: '200px',
+                  height: 'auto',
+                }
+              }
+            },
+            {
+              type: 'div',
+              props: {
+                style: {
+                  position: 'relative',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'flex-start',
+                  justifyContent: 'flex-end',
+                  padding: '60px',
+                  height: '100%',
+                },
+                children: [
+                  {
+                    type: 'h1',
+                    props: {
+                      style: {
+                        fontSize: '60px',
+                        fontWeight: 'bold',
+                        color: '#FFCB05',
+                        margin: '0 0 20px',
+                        lineHeight: 1.2,
+                        textShadow: '2px 2px 4px rgba(0,0,0,0.5)',
+                      },
+                      children: title
+                    }
+                  },
+                  {
+                    type: 'p',
+                    props: {
+                      style: {
+                        fontSize: '32px',
+                        color: 'white',
+                        margin: 0,
+                        lineHeight: 1.4,
+                        textShadow: '1px 1px 2px rgba(0,0,0,0.5)',
+                      },
+                      children: description
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      },
       {
         width: 1200,
         height: 630,
