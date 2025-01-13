82fb924ca4ec3cdb0e6fac48c81f3de3b8ef8147
Dan Newman <dan@dannewman.org>
2025-01-13 09:29:24 -0500

    edge functino v2

---
+++ netlify/edge-functions/og-image.tsx
@@ -0,0 +1,42 @@
+import React from "https://esm.sh/react@18.2.0";
+import { ImageResponse } from "https://esm.sh/og_edge/mod.ts"; 
+import type { Context } from '@netlify/edge-functions';
+
+export default async (req: Request, ctx: Context) => {
+  const url = new URL(req.url);
+  const img = url.searchParams.get('img') 
+             || '/social/og-default.jpg'; // fallback
+  const isCustomImage = img !== '/social/og-default.jpg';
+
+  const logoUrl = new URL('/robotics-og-logo.png', req.url).toString();
+  const backgroundUrl = new URL(img, req.url).toString();
+
+  return new ImageResponse(
+    <div style={{ width: 1200, height: 630, position: 'relative' }}>
+      <img
+        src={backgroundUrl}
+        style={{
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover'
+        }}
+      />
+      {isCustomImage && (
+        <img 
+          src={logoUrl}
+          style={{
+            position: 'absolute', top: 30, left: 30, width: 180
+          }}
+        />
+      )}
+    </div>,
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+};
+
+export const config = {
+  path: '/preview-image'
+};
