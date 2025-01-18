6a649d51616cd130cbb33b736fc836b49e1b6835
Dan Newman <dnewms@umich.edu>
2025-01-18 16:27:47 -0600

    proper og image generation for news

--- netlify/plugins/cache-og-images/index.js
+++ netlify/plugins/cache-og-images/index.js
@@ -1,12 +1,11 @@
-module.exports = {
-    onPreBuild: async ({ utils }) => {
-      // Restore cached OG images if they exist
-      await utils.cache.restore('.netlify/cache/og-images');
-      console.log('Restored OG image cache');
-    },
-    onPostBuild: async ({ utils }) => {
-      // Save newly generated OG images to cache
-      await utils.cache.save('.netlify/cache/og-images');
-      console.log('Saved OG images to cache');
-    },
-  };
+export const onPreBuild = async ({ utils }) => {
+  // Restore cached OG images if they exist
+  await utils.cache.restore('.netlify/cache/og-images');
+  console.log('Restored OG image cache');
+};
+
+export const onPostBuild = async ({ utils }) => {
+  // Save newly generated OG images to cache
+  await utils.cache.save('.netlify/cache/og-images');
+  console.log('Saved OG images to cache');
+};
